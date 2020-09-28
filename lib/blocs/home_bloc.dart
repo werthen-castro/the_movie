f58253030cc8a7dbf63722844a2c97c06e8fd16c
werthen-castro <wcastro.santos10@gmail.com>
2020-09-28 09:18:43 -0300

    adicionando offset tela home

--- lib/blocs/home_bloc.dart
+++ lib/blocs/home_bloc.dart
@@ -14,22 +14,32 @@ class HomeBloc {
   }
 
   ApiConnector _apiConnector = ApiConnector();
+
   final _movies = BehaviorSubject<List<Movie>>();
   get movies => _movies.stream;
 
-  getMoviesPopular() async {
-    List<Movie> temp = [];
+  final _offset = BehaviorSubject<int>.seeded(1);
+  get offset => _offset.stream;
+  List<Movie> temp = [];
+
+  Future<void> getMoviesPopular() async {
 
     try{
-      Response response =  await _apiConnector.apiGet('https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=pt-BR&page=1');
+      Response response =  await _apiConnector.apiGet('https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=pt-BR&page=${_offset.value}');
       Map body  = json.decode(response.body) ;
       body['results'].forEach((element) {
-        temp.add(Movie.fromMap(element) );
+        temp.add (Movie.fromMap(element) );
       });
 
       _movies.add(temp);
+      _offset.add(_offset.value + 1);
     }catch (e){
 
     }
+  }
+
+  dispose(){
+    _movies.close();
+    _offset.close();
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:the_movie/blocs/home_bloc.dart';
 import 'package:the_movie/models/movie_model.dart';
-import 'package:the_movie/utils/api_connector.dart';
 
 import '../keys.dart';
 
@@ -14,7 +10,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ScrollController _scrollController = ScrollController();
   HomeBloc _homeBloc = HomeBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+          _homeBloc.getMoviesPopular();
+      }
+    });
+  }
 
   _card(Movie movie) {
     return GestureDetector(
@@ -73,20 +80,27 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('The Movie'),
         ),
-        body: StreamBuilder<List<Movie>>(
-            stream: _homeBloc.movies,
-            builder: (BuildContext context, AsyncSnapshot<List<Movie>>  snapshot) {
-              if (snapshot.hasError) {}
+        body:  RefreshIndicator(
+            onRefresh: _homeBloc.getMoviesPopular,
+            child: StreamBuilder<List<Movie>>(
+              stream: _homeBloc.movies,
 
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return _card(snapshot.data[index]);
-                  });
-            }));
+              builder: (BuildContext context, AsyncSnapshot<List<Movie>>  snapshot) {
+                if (snapshot.hasError) {
+
+                }
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                    controller: _scrollController,
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return _card(snapshot.data[index]);
+                    });
+              }))
+        );
+
   }
 }
